3eb9ff7bb87b07457dad59f740f23576e972f101
aka3nd <fileaing1@gmail.com>
2025-12-06 23:00:19 +0700

    edit nonels

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,13 +7,21 @@ return {
 
 			null_ls.setup({
 				sources = {
+					--formatter
 					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.clang_format,
+
+					--linter
+					null_ls.builtins.diagnostics.cppcheck.with({
+						filetypes = { "c", "cpp", "objc", "objcpp" },
+						command = "/usr/local/bin/cppcheck",
+					}),
 				},
 			})
 
 			-- Keymap modern <leader>f untuk format
 			vim.keymap.set("n", "<leader>ff", function()
-			vim.lsp.buf.format()
+				vim.lsp.buf.format()
 			end, { noremap = true, silent = true })
 		end,
 	},
